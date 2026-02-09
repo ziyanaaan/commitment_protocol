c691fde2dc4f6191cf85540dd9cbe7970d112bc9
ziyanaaan <ktziyanamnu@gmail.com>
2026-02-10 00:20:45 +0530

    updated frontend public web adress to main.py

--- app/main.py
+++ app/main.py
@@ -37,6 +37,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://commitment-protocol.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
